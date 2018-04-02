a4bc27f8b4a016f23643401bc0f3c66c86dbf326
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:53:48 +0100

    Add raindrops

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+
+public static class Raindrops
+{
+    public static string Convert(int n)
+    {
+        var sb = new StringBuilder();
+        if (n % 3 == 0) sb.Append("Pling");
+        if (n % 5 == 0) sb.Append("Plang");
+        if (n % 7 == 0) sb.Append("Plong");
+        return sb.Length == 0 ? n.ToString() : sb.ToString();
+    }
+}
