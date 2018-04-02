c4e9ef7d5126225957e916db4fb70fb762e2d9c8
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:57:20 +0100

    Add strain

---
+++ strain/Strain.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public static class Strain
+{
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        foreach(T t in collection)
+        {
+            if (predicate(t)) yield return t;
+        }
+    }
+
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        foreach(T t in collection)
+        {
+            if (!predicate(t)) yield return t;
+        }
+    }
+}
