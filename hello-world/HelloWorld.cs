4790bdfb82be8361435f7a6f67b8547aece4f728
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:45:42 +0100

    Add hello-world

---
+++ hello-world/HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class HelloWorld
+{
+    public static String Hello(String input)
+    {
+        String s = input == null ? "World" : input;
+        return String.Format("Hello, {0}!", s);
+    }
+}
