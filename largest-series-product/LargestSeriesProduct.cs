1f30faf652412cd9f6742f65445c2f73cc12d3e8
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:46:52 +0100

    Add largest-series-product

---
+++ largest-series-product/LargestSeriesProduct.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class LargestSeriesProduct
+{
+    public static long GetLargestProduct(string s, int range) 
+    {
+        if (Regex.IsMatch(s, @"\D+") || range < 0 || range > s.Length)
+            throw new ArgumentException();
+
+        return Enumerable.Range(0, s.Length - range + 1)
+            .Select(SeriesProduct)
+            .Max();
+
+        long SeriesProduct(int n)
+        {
+            return s.Substring(n, range)
+                .Select(c => (long)Char.GetNumericValue(c))
+                .Aggregate(1L, (acc, v) => acc * v);
+        }
+    }
+}
