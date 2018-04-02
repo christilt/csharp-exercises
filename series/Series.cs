08cc710403af4074792f7b92d550e9c804a32a29
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:56:34 +0100

    Add series

---
+++ series/Series.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+
+public class Series
+{
+    public string Digits { get; private set; }
+
+    public Series(string digits) { Digits = digits; }
+
+    public int[][] Slices(int n)
+    {
+        if (n > Digits.Length) throw new ArgumentException();
+
+        return Enumerable.Range(0, Digits.Length - n + 1)
+            .Select(startIndex => Slice(startIndex, n))
+            .ToArray();
+    }
+
+    private int[] Slice(int startIndex, int length)
+    {
+        return Digits.Substring(startIndex, length)
+            .Select(c => (int)char.GetNumericValue(c))
+            .ToArray();
+    }
+}
