ceeb946658232de9371ebecc014471b7afabbd2e
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:54:33 +0100

    Add robot-name

---
+++ robot-name/RobotName.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Robot
+{
+    private static Random r = new Random();
+
+    public string Name { get; private set; }
+
+    public Robot() => Reset();
+
+    public void Reset()
+    {
+        char randLetter() => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[r.Next(26)];
+
+        Name  = $"{randLetter()}{randLetter()}{r.Next(1000):000}";
+    }
+}
