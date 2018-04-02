3d705eb12fbb40a9cfd3b2f0bfcf93db38497206
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:47:02 +0100

    Add leap

---
+++ leap/Leap.cs
@@ -0,0 +1,23 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        return year.isMultipleOf(4) && year.isNotMultipleOf(100)
+            || year.isMultipleOf(400);
+    }
+}
+
+public static class Extension
+{
+    public static bool isMultipleOf(this int a, int b)
+    {
+        return a % b == 0;
+    }
+
+    public static bool isNotMultipleOf(this int a, int b)
+    {
+        return a % b != 0;
+    }
+}
