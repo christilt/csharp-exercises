85a2c7245e12ac4c3f6bf1e832d3cf96f57810e4
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:44:12 +0100

    Add difference-of-squares

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+public static class Squares
+{
+    public static int SquareOfSums(int max)
+    {
+        return (int)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+    }
+
+    public static int SumOfSquares(int max)
+    {
+        return Enumerable.Range(1, max).Sum(x => x * x);
+    }
+
+    public static int DifferenceOfSquares(int max)
+    {
+        return SquareOfSums(max) - SumOfSquares(max);
+    }
+}
