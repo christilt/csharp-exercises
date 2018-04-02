5af70b5ad9bc2de26c9bf900046a5d3ec0869f55
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:46:10 +0100

    Add isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string input)
+    {
+        var s = String.Concat(input.Where(Char.IsLetter)).ToLower();
+        return s.All(c1 => s.Count(c2 => c1 == c2) == 1);
+    }
+}
