9fa26b86b046e78fd9088a6390f3d24833ada594
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:45:30 +0100

    Add hamming

---
+++ hamming/Hamming.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Compute(string a, string b)
+    {
+        if (a.Length != b.Length) throw new ArgumentException();
+        return a.Zip(b, (a1, b1) => a1 == b1 ? 0 : 1).Sum();
+    }
+}
