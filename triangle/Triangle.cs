9356af0c9704cb727ef667f017b564d41d670840
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:57:57 +0100

    Add triangle

---
+++ triangle/Triangle.cs
@@ -0,0 +1,23 @@
+using System;
+using static TriangleKind;
+
+public enum TriangleKind
+{
+    Equilateral, Isosceles, Scalene
+}
+
+public static class Triangle
+{
+    public static TriangleKind Kind(decimal a, decimal b, decimal c)
+    {
+        if (Illogical()) throw new TriangleException();
+        if (a == b && b == c) return Equilateral;
+        if (a == b || b == c || c == a) return Isosceles;
+        else return Scalene;
+
+        bool Illogical() => (a <= 0 || b <= 0 || c <= 0) ||
+                (a + b <= c || b + c <= a || c + a <= b);
+    }
+}
+
+public class TriangleException : Exception { }
