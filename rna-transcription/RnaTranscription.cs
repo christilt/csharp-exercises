f0fff4a304bd4f034ace3bdbdafae51cc1f8ff5d
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:54:23 +0100

    Add rna-transcription

---
+++ rna-transcription/RnaTranscription.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Complement
+{
+    static Dictionary<char, char> trans = new Dictionary<char, char>()
+    {
+        { 'G', 'C' },
+        { 'C', 'G' },
+        { 'T', 'A' },
+        { 'A', 'U' }
+    };
+
+    public static string OfDna(string nucleotide)
+    {
+        return String.Concat(nucleotide.Select(x => trans[x]));
+    }
+}
