b04168c51fa687875f0a6160f0a7e15228807e18
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:41:03 +0100

    Add binary-search

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+public static class BinarySearch
+{
+    public static int Search(int[] input, int target)
+    {
+        var idxValPairs = Enumerable.Range(0, input.Length)
+            .Zip(input, Tuple.Create)
+            .OrderBy(iv => iv.Item2)
+            .ToArray();
+        while (idxValPairs.Length != 0)
+        {
+            var mid = idxValPairs.Length / 2;
+            var midIdxVal = idxValPairs[mid];
+            if (midIdxVal.Item2 == target) return midIdxVal.Item1;
+            idxValPairs = target > midIdxVal.Item2
+                  ? idxValPairs.Skip(mid+1).ToArray()
+                  : idxValPairs.Take(mid).ToArray();
+        }
+        return -1;
+    }
+}
