ca3ad2e484d0c7d87e3d42db21233430b77a90ec
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:44:25 +0100

    Add error-handling

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,27 @@
+using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        return int.TryParse(input, out int value) ? value : (int?)null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return int.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception();
+        }
+    }
+}
