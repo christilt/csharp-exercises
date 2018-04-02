7d7cc4d3c6e2621bd691f4120c57775c829ca90d
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:48:28 +0100

    Add phone-number

---
+++ phone-number/PhoneNumber.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+
+public class PhoneNumber
+{
+    private const string Invalid = "0000000000";
+    private string number;
+
+    public PhoneNumber(string input)
+    {
+        Number = input;
+    }
+
+    public string Number
+    {
+        get => number;
+        private set => number = Filter(Clean(value));
+    }
+
+    public string AreaCode { get => Number.Substring(0, 3); }
+
+    public string ExchangeCode { get => Number.Substring(3, 3); }
+
+    public string LineNumber { get => Number.Substring(6, 4); }
+
+    public override string ToString()
+    {
+        return $"({AreaCode}) {ExchangeCode}-{LineNumber}";
+    }
+
+    private string Clean(string s)
+    {
+        return String.Concat(s.Where(Char.IsDigit));
+    }
+
+    private string Filter(string s)
+    {
+        if (s.Length < 10) return Invalid;
+        if (s.Length > 11) return Invalid;
+        if (s.Length == 10) return s;
+        return s.StartsWith("1") ? s.Substring(1) : Invalid;
+    }
+}
