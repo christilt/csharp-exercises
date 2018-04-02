f815a4a345118607eba79c0b6dc7c2377e7eb8e4
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:42:52 +0100

    Add book-store

---
+++ book-store/BookStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class BookStore
+{
+    public static double Total(IEnumerable<int> books)
+    {
+        var groupings = _Total(books);
+        return 7;
+    }
+
+    private static IEnumerable<object> _Total(IEnumerable<int> books)
+    {
+        return books
+            .GroupBy(i => i)
+            .Select(g => new { Metric = g.Key, Count = g.Count() })
+            .OrderBy(g => g.Metric);
+    }
+}
