a1b194060f7aafc10609c6ff84500f660247e978
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:47:59 +0100

    Add pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var s = input.ToLower();
+        return "abcdefghijklmnopqrstuvwxyz".All(s.Contains);
+    }
+}
