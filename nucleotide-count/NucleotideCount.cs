564f7750e3f3a3caf7177f019b73dd9c74431c60
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:47:49 +0100

    Add nucleotide-count

---
+++ nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DNA
+{
+    private const string nucleotides = "ACGT";
+    private string seq;
+
+    public DNA(string seq)
+    {
+        this.seq = seq;
+    }
+
+    public IDictionary<char, int> NucleotideCounts
+    {
+        get
+        {
+            return nucleotides.ToDictionary(x => x, Count);
+        }
+    }
+
+    public int Count(char n)
+    {
+        if (!nucleotides.Contains(n))
+            throw new InvalidNucleotideException();
+        return seq.Where(x => x == n).Count();
+    }
+}
+
+public class InvalidNucleotideException : Exception { }
