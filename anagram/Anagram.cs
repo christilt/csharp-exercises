70c429cc342f96017608b72e48a95b03c366f454
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:34:01 +0100

    Add anagram

---
+++ anagram/Anagram.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+
+public class Anagram
+{
+    public string Word { get; }
+
+    public Anagram(string word)
+    {
+        Word = word;
+    }
+
+    public string[] Anagrams(string[] inputs)
+    {
+        return inputs
+            .Where(IsNotIdentical)
+            .Where(IsAnagram)
+            .ToArray();
+    }
+
+    private bool IsNotIdentical(string input)
+    {
+        return Word.ToLower() != input.ToLower();
+    }
+
+    private bool IsAnagram(string input)
+    {
+        return LetterCountsMatch(Word, input)
+            && LetterCountsMatch(input, Word);
+
+        bool LetterCountsMatch(string a, string b)
+        {
+            a = a.ToLower();
+            b = b.ToLower();
+            return a.All(c => a.Count(ac => c == ac)
+                           == b.Count(bc => c == bc));
+        }
+    }
+}
