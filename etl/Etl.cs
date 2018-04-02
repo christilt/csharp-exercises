94c11438fe6ab39b58eb09815c308fc5a8a26774
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:44:35 +0100

    Add etl

---
+++ etl/Etl.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ETL
+{
+    public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
+    {
+        return old
+            .SelectMany(kv => kv.Value
+                       .ToDictionary(v => v, v => kv.Key))
+            .ToDictionary(kv => kv.Key.ToLower(), kv => kv.Value);
+    }
+}
