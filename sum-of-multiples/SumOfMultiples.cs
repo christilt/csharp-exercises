1ba6904b69d6397a91afdc2f8f997c56a0410614
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:57:38 +0100

    Add sum-of-multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> facts, int max)
+    {
+        return Enumerable.Range(1, max-1)
+            .Where(mult => anyMultiplesOf(facts, mult))
+            .Sum();
+    }
+
+    static bool anyMultiplesOf(IEnumerable<int> facts, int mult)
+    {
+        return facts.Any(fact => isMultipleOf(fact, mult));
+    }
+
+    static bool isMultipleOf(int fact, int mult)
+    {
+        return mult % fact == 0;
+    }
+}
