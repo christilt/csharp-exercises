b0894813e55ff60d37b50295d861549421a9cda9
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:33:02 +0100

    Add allergies

---
+++ allergies/Allergies.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Allergies
+{
+    private static Dictionary<string, int> bits =
+        new Dictionary<string, int>
+        {
+            { "eggs", 0 }, { "peanuts", 1 }, { "shellfish", 2 },
+            { "strawberries", 3 }, { "tomatoes", 4 },
+            { "chocolate", 5 }, { "pollen", 6 }, { "cats", 7 }
+        };
+
+    private int mask;
+
+    public Allergies(int mask) { this.mask = mask; }
+
+    public bool AllergicTo(string a) => bitIsOne(mask, bits[a]);
+
+    public IList<string> List()=> bits.Keys.Where(AllergicTo).ToList();
+
+    private static bool bitIsOne(int i, int b) => ((i >> b) & 1) == 1;
+}
