41cd53a0b3a3ec8594aeabbea689f469b763dbe7
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:56:44 +0100

    Add sieve

---
+++ sieve/Sieve.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Sieve
+{
+    public static int[] Primes(int limit)
+    {
+        var primes = new List<int>();
+        var composites = new HashSet<int>();
+        for (int factor = 2; factor <= limit; factor++)
+        {
+            if (composites.Contains(factor)) continue;
+            primes.Add(factor);
+            for (int product = factor * 2; product <= limit; product += factor)
+            {
+                composites.Add(product);
+            }
+        }
+        return primes.ToArray();
+    }
+}
