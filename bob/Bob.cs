6a7469b8551dc3c00aeb7fb36898f0a42f3a1c8d
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:42:40 +0100

    Add bob

---
+++ bob/Bob.cs
@@ -0,0 +1,25 @@
+using System;
+
+public static class Bob
+{
+    public static string Hey(string input)
+    {
+        var s = input.Trim();
+        if (s == "")
+        {
+            return "Fine. Be that way!";
+        }
+        else if (s.ToLower() != s.ToUpper() && s == s.ToUpper())
+        {
+            return "Whoa, chill out!";
+        }
+        else if (s.EndsWith("?"))
+        {
+            return "Sure.";
+        }
+        else
+        {
+            return "Whatever.";
+        }
+    }
+}
