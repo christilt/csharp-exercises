e59c29f25c4688dcdfaa6f1611b802a3d2df28d9
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:43:55 +0100

    Add collatz-conjecture

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int n)
+    {
+        if (n < 1) throw new ArgumentException();
+        var steps = 0;
+        while (n != 1)
+        {
+            n = Next(n);
+            steps++;
+        }
+        return steps;
+    }
+
+    private static int Next(int n)
+    {
+        return n % 2 == 0 ? n / 2 : 3 * n + 1;
+    }
+}
