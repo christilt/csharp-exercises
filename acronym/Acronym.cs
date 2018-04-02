62e2118ac641645dad389dbd3f8002357eedb507
christilt <tiltchris+gh@gmail.com>
2018-04-02 16:18:24 +0100

    Add acronym

---
+++ acronym/Acronym.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string x)
+    {
+        var abbreviation = Enumerable.Range(0, x.Length)
+            .Where(i => i == 0 || isCapitalIndex(i))
+            .Select(i => x.ToUpper()[i]);
+        return String.Concat(abbreviation);
+
+        bool isCapitalIndex(int i)
+        {
+            var split = x.Substring(i - 1, 2);
+            return Regex.IsMatch(split, "[^a-zA-Z][a-zA-Z]") ||
+                Regex.IsMatch(split, "[a-z][A-Z]");
+        }
+    }
+}
